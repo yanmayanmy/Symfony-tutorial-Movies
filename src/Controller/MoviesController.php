cba7d75810d5acaa7ba6e13678707845528bf3c2
yanmayanmy <yanmy.yamauchi@karabiner.tech>
2023-07-26 16:33:47 +0900

    [add]Block, statements, and global variables.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -12,8 +12,12 @@ class MoviesController extends AbstractController
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        return $this->render('index.html.twig', [
-            'title' => '',
-        ]);
+        $movies = ["Howl's Moving Castle", "Spirited Away"];
+        $dramas = ["Orange is the New Black", "Move to Heaven"];
+
+        return $this->render('index.html.twig', array(
+            'movies' => $movies,
+            'dramas' => $dramas,
+        ));
     }
 }
