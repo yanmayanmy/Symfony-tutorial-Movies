3bbd4a045249f35a0fecbd01223c5ca70a0db626
yanmayanmy <yanmy.yamauchi@karabiner.tech>
2023-08-04 14:10:26 +0900

    [edit]Playing around with some methods. #14

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,15 +12,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        $movies = ["Howl's Moving Castle", "Spirited Away"];
-        $dramas = ["Orange is the New Black", "Move to Heaven"];
+        // findAll() - SELECT * FROM movie
+        // find(9) - SELECT * FROM movie WHERE id=9
+        // findBy() - SELECT * FROM movie ORDER BY id DESC
+        // findOneBy() - SELECT * FROM movie WHERE ID = 10 AND title = 'avengers' ORDER BY id DESC
+        // count() - SELECT COUNT() FROM movie WHERE id = 9
+        // getClassName() - returns class name e.g. "App\Entity\Movie"
 
-        return $this->render('index.html.twig', array(
-            'movies' => $movies,
-            'dramas' => $dramas,
-        ));
+        $repository = $this->em->getRepository(Movie::class);
+
+        $movies = $repository->getClassName();
+
+        dd($movies);
+        return $this->render('index.html.twig');
     }
 }
