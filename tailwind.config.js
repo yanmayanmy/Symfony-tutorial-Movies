cf6f29cb30eece21b22ac342518e51dcf3ff7961
yanmayanmy <yanmy.yamauchi@karabiner.tech>
2023-08-09 15:19:52 +0900

    [add]config files. #16
    For postcss & tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.{vue,js,ts,jsx,tsc}",
+    "./templates/**/*.{html,twig}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
