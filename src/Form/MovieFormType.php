ede5a359a12dcbf0682284ce620e3b9990d21913
yanmayanmy <yanmy.yamauchi@karabiner.tech>
2023-08-16 10:32:11 +0900

    [add]make:form. #19

---
+++ src/Form/MovieFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('releaseYear')
+            ->add('description')
+            ->add('imagePath')
+            ->add('actors')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
